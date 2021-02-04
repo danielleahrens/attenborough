f8fe33c8c215429756160fff4381130df003a179
Danielle Ahrens <danielle.c.ahrens@gmail.com>
2021-02-04 13:44:23 -0500

    added graph to detailed metric page
    had to downgrade react to get it to work

--- src/MetricDetail.js
+++ src/MetricDetail.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {
+  LineChart, XAxis, YAxis, Tooltip, CartesianGrid, Line, Legend
+} from 'recharts';
 import './MetricDetail.css';
 
 class MetricDetail extends Component {
@@ -16,7 +19,7 @@ class MetricDetail extends Component {
           this.setState({
             isMetricsLoaded: true,
             sensorMetrics: result.items
-          });
+          }, () => {this.structureData()});
         },
         (error) => {
           this.setState({
@@ -27,22 +30,59 @@ class MetricDetail extends Component {
       )
   }
 
+  structureData() {
+    var data = {}
+    console.log('heres state', this.state)
+    Object.keys(this.state.sensorMetrics[0]['measurement']).map((measurement) => {
+      data[[measurement]] = []
+      this.state.sensorMetrics[0]['measurement'][[measurement]].map((dataPoint) => {
+        if (dataPoint[2]) {
+          var vertex = {'name': dataPoint[0], 'pt': dataPoint[2].toFixed(2)}
+          if (this.state.sensorMetrics[0]['alert']) {
+            vertex['alertHigh'] = this.state.sensorMetrics[0]['alert'][[measurement]]['upper']
+            vertex['alertLow'] = this.state.sensorMetrics[0]['alert'][[measurement]]['lower']
+          }
+          data[[measurement]].unshift(vertex)
+        }
+      })
+    })
+    console.log('heres the data object', data)
+    this.setState({data: data})
+  }
+
   render() {
     const { error, isMetricsLoaded } = this.state
     if (error) {
       return <div>Error: {error.message}</div>
-    } else if (!isMetricsLoaded && !this.state.locations) {
+    } else if (!isMetricsLoaded && !this.state.locations || !this.state.data) {
       return <div>Loading...</div>
     } else {
       return (
         <div>
-          {console.log('this is the metrics', this.state.sensorMetrics)}
+          {console.log('this is the metrics', this.state)}
           <div>You Made it to the Detailed Metrics for the {this.props.sensor['sensor_id']} Sensor</div>
-          {Object.keys(this.state.sensorMetrics[0]['measurement']).map((measurement) => {
+          {Object.keys(this.state.data).map((measurement) => {
+            var data = this.state.data
             return(
               <div>
                 <div>{measurement}</div>
                 <div>The Graph</div>
+
+                  <LineChart
+                    width={1700}
+                    height={400}
+                    data={data[[measurement]]}
+                    margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+                  >
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <Legend />
+                    <Line type="monotone" dataKey="pt" stroke="#ff7300" yAxisId={0} />
+                    <Line type="monotone" dataKey="alertHigh" stroke="#8884d8" yAxisID={0}/>
+                    <Line type="monotone" dataKey="alertLow" stroke="#8884d8" yAxisID={0}/>
+                  </LineChart>
               </div>
             )
           })}

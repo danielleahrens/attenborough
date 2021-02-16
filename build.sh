dd73953d22dd75633534b8e1e38b7434715358df
Danielle Ahrens <danielle.c.ahrens@gmail.com>
2021-02-16 11:52:23 -0500

    updating nginx docker run command to match cenozoa

--- build.sh
+++ build.sh
@@ -15,5 +15,5 @@ ssh -T ubuntu@192.168.1.10 << EOF
   echo "docker kill ui nginx"
   sudo docker kill nginx
   echo "docker run nginx"
-  sudo docker run -v /opt/cenozoa/nginx.conf:/etc/nginx/conf.d/default.conf -v /opt/cenozoa/.htpasswd:/etc/apache2/.htpasswd -v /opt/cenozoa/.supasswd:/etc/apache2/.supasswd -v /etc/letsencrypt:/etc/ssl/letsencrypt -v /opt/cenozoa/ui:/www -p 443:443 -p 80:80 --network=cenozoanet --name=nginx --rm -d nginx
+  sudo docker run -v /opt/cenozoa/nginx.conf:/etc/nginx/conf.d/default.conf -v /opt/cenozoa/.htpasswd:/etc/apache2/.htpasswd -v /opt/cenozoa/.supasswd:/etc/apache2/.supasswd -v /opt/cenozoa/.senpasswd:/etc/apache2/.senpasswd -v /etc/letsencrypt:/etc/ssl/letsencrypt -v /opt/cenozoa/ui:/www -p 443:443 -p 80:80 --network=cenozoanet --name=nginx --rm -d nginx
 EOF
